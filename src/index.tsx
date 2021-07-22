4006eba016aca4b45084b0b3508b4ca5e91bd59e
luoshuitaotao <jcui@unomaha.edu>
2021-07-22 09:48:15 -0500

    learn a different way/class components for User Search

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import  ReactDOM  from "react-dom";
 // import GuestList from "./state/GuestList";
-import UserSearch from './state/UserSearch';
+// import UserSearch from './state/UserSearch';
+import EventComponent from './events/EventComponent';
 
 const App= ()=>{
     return <div>
-        <UserSearch />
+        <EventComponent />
     </div>
 };
 ReactDOM.render(
